7e7bf9114a7aeb71d1190db02901e7025fd64d49
Daniel Correia <dduarte96@hotmail.com>
2018-03-08 15:44:58 +0000

    Fixed generic method calls with arguments

--- newLabs/Lab2/src/main/java/pt/ist/ap/labs/Shell.java
+++ newLabs/Lab2/src/main/java/pt/ist/ap/labs/Shell.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.lang.System.out;
 
@@ -11,6 +12,30 @@ public class Shell {
     private static Object previous = null;
     private static Scanner sc = new Scanner(System.in);
     private static Map<String,Object> objectMap = new HashMap<>();
+
+    private static final Map<String, Class<?>> PRIMITIVES_MAP = new HashMap<>(){{
+        put("int", Integer.class);
+        put("double", Double.class);
+        put("float", Float.class);
+        put("char", Character.class);
+        put("boolean", Boolean.class);
+        put("byte", Byte.class);
+        put("long", Long.class);
+        put("short", Short.class);
+        put("void", Void.class);
+    }};
+
+    private static final Map<String, Class<?>> PRIMITIVES_CLASSES = new HashMap<>(){{
+        put("int", int.class);
+        put("double", double.class);
+        put("float", float.class);
+        put("char", char.class);
+        put("boolean", boolean.class);
+        put("byte", byte.class);
+        put("long", long.class);
+        put("short", short.class);
+        put("void", void.class);
+    }};
 
     public static void main(String... args){
         final Map<String, Runnable> commands = new HashMap<>(){{
@@ -32,14 +57,16 @@ public class Shell {
                     commands.get(command).run();
             }
             catch (Exception e) {
-                out.println(e.getMessage());
+                out.printf("SOME ERROR: %s",e.getMessage());
             }
             printPreviousResult();
         }
     }
 
     private static void setVariable() {
-        objectMap.put(sc.next(), previous);
+        String key = sc.next();
+        objectMap.put(key, previous);
+        previous = objectMap.get(key);
         out.printf("Saved name for object of type %s%n", previous.getClass());
     }
 
@@ -56,7 +83,7 @@ public class Shell {
             Arrays.stream((Object[]) previous).forEach(out::println);
         }
         else {
-            out.println(previous);
+            out.println(previous.toString());
         }
     }
 
@@ -86,11 +113,15 @@ public class Shell {
             try {
                 Object result = method.invoke(aPackage);
 
-                if (result == null)
+                if (result == null) {
                     out.printf("%s: Result Non Defined%n", method.getName());
-                else
+                }
+                else if (result.getClass().isArray() && ((Object[])result).length == 0){
+                    out.printf("%s: %n", method.getName());
+                }
+                else {
                     out.printf("%s: %s%n", method.getName(), result);
-
+                }
             } catch (IllegalAccessException | InvocationTargetException e) {
                 e.printStackTrace();
             }
@@ -100,12 +131,68 @@ public class Shell {
     private static void callGenericCommand(String command) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         out.println("Trying generic command " + command);
         String method_args = sc.nextLine();
-        Method method = Objects.requireNonNull(previous).getClass().getMethod(command);
         if (method_args.isEmpty()){
+            Method method = Objects.requireNonNull(previous).getClass().getMethod(command);
             previous = method.invoke(previous);
         }
         else {
-            previous = method.invoke(previous, (Object) method_args.split(" "));
+            // only accept one int for now
+            String[] args = method_args.trim().split(" ");
+
+            if (args.length % 2 == 0){
+                Class[] types = getTypesFromArgs(args);
+
+                Method method;
+                if (previous instanceof Class<?>){
+                    method = ((Class) previous).getMethod(command, types);
+                }
+                else{
+                    method = Objects.requireNonNull(previous).getClass().getMethod(command, types);
+                }
+
+                Object[] params = getParamsFromArgs(args);
+
+                previous = method.invoke(previous, params);
+            }
         }
+    }
+
+    private static Object[] getParamsFromArgs(String[] args) {
+        return IntStream.range(0, args.length)
+                            .filter(i -> i % 2 != 0)
+                            .mapToObj(i -> {
+                                if (PRIMITIVES_MAP.containsKey(args[i-1])){
+                                    try {
+                                        return PRIMITIVES_MAP.get(args[i-1]).getMethod("valueOf", String.class).invoke(PRIMITIVES_MAP.get(args[i-1]), args[i]);
+                                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                try {
+                                    return Class.forName(args[i - 1]).getConstructor(String.class).newInstance(args[i]);
+                                } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+                                    e.printStackTrace();
+                                }
+                                return null;
+                            })
+                            .toArray();
+    }
+
+    private static Class[] getTypesFromArgs(String[] args) {
+        return IntStream.range(0, args.length)
+                            .filter(i -> i % 2 == 0)
+                            .mapToObj(i -> {
+                                if (PRIMITIVES_CLASSES.containsKey(args[i])){
+                                    return PRIMITIVES_CLASSES.get(args[i]);
+                                }
+                                else{
+                                    try {
+                                        return Class.forName(args[i]);
+                                    } catch (ClassNotFoundException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                return null;
+                            }).toArray(Class[]::new);
     }
 }
