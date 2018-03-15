1283f1d4bca6ef83309c84793a81cf1448bbf66e
Daniel Correia <dduarte96@hotmail.com>
2018-03-15 12:31:07 +0000

    finished lab3

--- newLabs/Lab3/src/main/java/RunTests.java
+++ newLabs/Lab3/src/main/java/RunTests.java
@@ -1,19 +1,72 @@
-import java.lang.reflect.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RunTests {
-   public static void main(String[] args) throws Exception {
-      int passed = 0, failed = 0;
-      for (Method m : Class.forName(args[0]).getMethods()) {
-         if (m.isAnnotationPresent(Test.class)) {
-            try {
-               m.invoke(null);
-               passed++;
-            } catch (Throwable ex) {
-               System.out.printf("Test %s failed: %s %n", m, ex.getCause());
-               failed++;
+    static Map<String, Method> setups = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+
+
+        Class superclass = Class.forName(args[0]).getSuperclass();
+        while (!(superclass.getSimpleName().equals("Object"))){
+            processSetupAnnotations(superclass);
+            processTestAnnotations(superclass);
+            superclass = superclass.getSuperclass();
+        }
+
+        Class targetclass = Class.forName(args[0]);
+        processSetupAnnotations(targetclass);
+        processTestAnnotations(targetclass);
+    }
+
+    private static void processSetupAnnotations(Class clazz) {
+        for (Method m: clazz.getDeclaredMethods()){
+            if (m.isAnnotationPresent(Setup.class)){
+                //System.out.println("process Setup annotation");
+                m.setAccessible(true);
+                setups.put(m.getAnnotation(Setup.class).value(),m);
             }
-         }
-      }
-      System.out.printf("Passed: %d, Failed %d%n", passed, failed);
-   }
+        }
+    }
+
+    private static void processTestAnnotations(Class clazz) {
+        int passed = 0, failed = 0;
+
+        for (Method m : clazz.getDeclaredMethods()) {
+            if (m.isAnnotationPresent(Test.class) && Modifier.isStatic(m.getModifiers())) {
+                try {
+                    m.setAccessible(true);
+                    // check value
+                    String[] testValue = m.getAnnotation(Test.class).value();
+                    //System.out.println(testValue[0]);
+                    if (testValue[0].equals("*")){
+                        setups.forEach((s, method) -> {
+                            try {
+                                method.invoke(null);
+                            } catch (IllegalAccessException | InvocationTargetException e) {
+                                throw new RuntimeException();
+                            }
+                        });
+                    }
+                    else {
+                        for (String s : testValue) {
+                            setups.get(s).invoke(null);
+                        }
+                    }
+
+                    m.invoke(null);
+                    passed++;
+                    System.out.printf("Test %s OK!%n", m);
+                } catch (Throwable ex) {
+                    System.out.printf("Test %s failed%n", m);
+                    failed++;
+                }
+            }
+        }
+        System.out.printf("Passed: %d, Failed %d%n", passed, failed);
+    }
 }
