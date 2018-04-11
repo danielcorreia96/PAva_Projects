9fda1eadc58fdb74903a34b23f13249c21ad577d
Daniel Correia <dduarte96@hotmail.com>
2018-04-11 15:15:53 +0100

    Prepared project for before/after methods dev phase

---
+++ project1/src/main/java/examples/ExplainApp.java
@@ -0,0 +1,40 @@
+package examples;
+
+import meic.ist.pa.GenericFunctions.GenericFunction;
+import meic.ist.pa.GenericFunctions.BeforeMethod;
+import meic.ist.pa.GenericFunctions.AfterMethod;
+
+@GenericFunction
+interface Explain {
+    public static void it(Integer i) {
+        System.out.print(i + " is an integer");
+    }
+
+    public static void it(Double i) {
+        System.out.print(i + " is a double");
+    }
+
+    public static void it(String s) {
+        System.out.print(s + " is a string");
+    }
+
+    @BeforeMethod
+    public static void it(Number n) {
+        System.out.print("The number ");
+    }
+
+    @AfterMethod
+    public static void it(Object o) {
+        System.out.println(".");
+    }
+}
+
+public class ExplainApp {
+
+    public static void main(String[] args) {
+        Object[] objs = new Object[] { "Hello", 1, 2.0 };
+        for (Object o : objs) {
+            Explain.it(o);
+        }
+    }
+}
